92a84577a124680b8f5df8ccc1c60232534dbd72
DavMunHer <davidmunyozh@gmail.com>
2025-06-25 20:30:04 +0200

    feat: implementation of the carousel component is now working

---
+++ projects/carousel-app/src/app/components/carousel-implementation/carousel-implementation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CarouselComponent } from 'carousel';
+
+@Component({
+  selector: 'carousel-implementation',
+  imports: [CarouselComponent],
+  templateUrl: './carousel-implementation.component.html',
+  styleUrl: './carousel-implementation.component.css'
+})
+export class CarouselImplementationComponent {
+
+}
